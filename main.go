9da3b930989d8c2398fd480ea942a8427eb5e351
Steve Winton <stevewinton@gmail.com>
2020-09-18 14:53:07 -0500

    Implement pull-request-bot

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"log"
+	"strings"
 
 	"github.com/google/go-github/github"
 	"github.com/swinton/go-probot/probot"
@@ -15,5 +17,32 @@ func main() {
 
 		return nil
 	})
+
+	probot.HandleEvent("pull_request", func(ctx *probot.Context) error {
+		event := ctx.Payload.(*github.PullRequestEvent)
+		pr := event.GetPullRequest()
+		repo := event.GetRepo()
+		log.Printf("Got the PR title: %s", *pr.Title)
+
+		newReview := &github.PullRequestReviewRequest{CommitID: pr.Head.SHA}
+		if strings.Contains(*pr.Title, "🤖") {
+			newReview.Event = github.String("APPROVE")
+			newReview.Body = github.String("LGTM 🚀")
+		} else {
+			newReview.Event = github.String("REQUEST_CHANGES")
+			newReview.Body = github.String("Needs more 🤖")
+		}
+
+		review, _, err := ctx.GitHub.PullRequests.CreateReview(context.Background(), *repo.Owner.Login, *repo.Name, pr.GetNumber(), newReview)
+
+		if err != nil {
+			return nil
+		}
+
+		log.Printf("New review created: %+v", review)
+
+		return nil
+	})
+
 	probot.Start()
 }
